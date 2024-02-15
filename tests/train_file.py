2410d53dc23684928adcba6ea12a771c21970a4c
Enora <erice1@brynmawr.edu>
2024-02-15 16:36:03 -0700

    modifications to allow comparison to original langrank

--- tests/train_file.py
+++ tests/train_file.py
@@ -3,25 +3,32 @@ import sys, getopt
 import os, sys
 root=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root)
-from langrank import prepare_train_file_no_data, train_from_pickle
+from langrank import prepare_train_file_no_data, train_from_pickle, prepare_train_pickle_no_data, train
 import pickle 
 
 
-def train_one(dir_path, task, langs, rank, test_lang):
-    prepare_train_file_no_data(langs=langs, rank=rank, tmp_dir=dir_path, task = task, distances = False)
+def train_one_distanceless(dir_path, task, langs, rank, test_lang):
+    prepare_train_pickle_no_data(langs=langs, rank=rank, tmp_dir=dir_path, task = task, distances = False)
     output_model = "{}/{}.txt".format(dir_path,test_lang)
     train_from_pickle(tmp_dir= dir_path, output_model=output_model)
     assert os.path.isfile(output_model)
 
-
+def train_one_distances(dir_path, task, langs, rank, test_lang):
+    prepare_train_file_no_data(langs=langs, rank=rank, tmp_dir=dir_path, task = task)
+    output_model = "{}/{}.txt".format(dir_path,test_lang)
+    train(tmp_dir= dir_path, output_model=output_model)
+    assert os.path.isfile(output_model)
 
 def main(argv):
  
     task = ''
-    opts, args = getopt.getopt(argv,"t:",["task"])
+    dist = False
+    opts, args = getopt.getopt(argv,"t:d",["task", "distances"])
     for opt, arg in opts:
       if opt in ("-t", "--task"):
          task = arg
+    if opt in ("-d", "--distances"):
+         dist = True
 
     t_file = "./training-data/{task}_original_ranked_train_no_ties.pkl".format(task = task)
     print(t_file)
@@ -35,9 +42,12 @@ def main(argv):
         print(lang)
         languages = list(training[lang][1].keys())
         rank = list(training[lang][1].values())
-        train_one(model_dir, task, languages, rank, lang)
+        if dist:
+            train_one_distances(model_dir, task, languages, rank, lang)
+        else:
+            train_one_distanceless(model_dir, task, languages, rank, lang)
     
-        print("finished training {task}".format(task = task))
+    print("finished training {task}".format(task = task))
 
 if __name__ == "__main__":
    main(sys.argv[1:])
